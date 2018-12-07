8b7ea02c97a801b5aebaa1ea5a69370c3dde4169
Benedikt D Valdez <benediktvaldez@gmail.com>
2018-12-07 13:45:11 +0000

    Fix server configuration

--- server.js
+++ server.js
@@ -2,6 +2,6 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 9000
 
-app.use(express.static('dist'))
+app.use('/', express.static('dist'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
